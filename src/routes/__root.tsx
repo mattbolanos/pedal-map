f8126e56c15e1db49bc2119ea5044264e8dc77e3
mattbolanos <matthew.a.bolanos@gmail.com>
2026-04-23 15:31:41 -0400

    welcome back react scan

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -51,9 +51,15 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning className="dark">
       <head>
+        {process.env.REACT_SCAN_ENABLED === "true" && (
+          <script
+            async
+            crossOrigin="anonymous"
+            src="//unpkg.com/react-scan/dist/auto.global.js"
+          />
+        )}
         {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <theme came with the template> */}
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
-
         <HeadContent />
       </head>
       <body className="bg-background font-sans wrap-anywhere antialiased">
